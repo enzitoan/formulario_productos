c2316741ddd8eb5c600ea87ae2f9406ead790502
enzitoan <ahumada.enzo@gmail.com>
2017-05-29 12:46:46 -0400

    Cambio Version

--- form_productos.php
+++ form_productos.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php echo "version 1.0.1" ?></title>
+	<title><?php echo "version 1.0.2" ?></title>
 	<!-- Latest compiled and minified CSS & JS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery.js"></script>
