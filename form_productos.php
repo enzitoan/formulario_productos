b89154bc69d9e4707872cdba3ddab40b6330a93f
Enzo Ahumada <enzitoan@users.noreply.github.com>
2017-05-29 12:18:35 -0400

    actualiza version prueba

--- form_productos.php
+++ form_productos.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php echo "version 1.0.0" ?>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
+	<title><?php echo "version 1.0.1" ?></title>
 	<!-- Latest compiled and minified CSS & JS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery.js"></script>
