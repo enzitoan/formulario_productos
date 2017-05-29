06a0bb1289b2441e11f72d8607ecdb48c7a18bd7
enzitoan <ahumada.enzo@gmail.com>
2017-05-29 16:42:20 -0400

    prueba rev

--- form_productos.php
+++ form_productos.php
@@ -1,9 +1,12 @@
+<?php 
+	$rev = exec('git rev-parse --short HEAD');
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title><?php echo "version 1.0.2" ?></title>
+	<title><?php echo "version 1.0.0.".$rev ?></title>
 	<!-- Latest compiled and minified CSS & JS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery.js"></script>
